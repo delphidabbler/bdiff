0176edf04a886e5e9a79ebf4eb930b26f465bc7b
DelphiDabbler <github@delphidabbler.com>
2009-08-01 15:22:31 +0000

    Complete re-write: * Now four different tests are supported: binary patch (as before), quoted diff, filtered diff and program version info. * Option added to delete temporary files generated by tests. * User can now specify location of BDiff and BPatch programs being tested.

--- Test/Test.bat
+++ Test/Test.bat
@@ -1,23 +1,12 @@
 @rem ---------------------------------------------------------------------------
-@rem Script used to create and test patch files created by BDiff and applied by
-@rem BPatch.
+@rem Script used to run tests on BDiff and BPatch.
 @rem
-@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2007-2008
+@rem See file ReadMe.txt for a description of available tests and requirements.
 @rem
-@rem Requires files named Test1 and Test2 in same directory as batch file. These
-@rem files should be different.
-@rem Also requires that BDiff.exe and BPatch.exe should be located in the
-@rem ..\Exe directory, relative to the location of Test.bat.
-@rem 
-@rem Runs BDiff on Test1 and Test2 to create patch file Patch. Then runs BPatch
-@rem on Test1 and Patch to create Test3 which should be a recreation of Test2.
-@rem Finally runs Windows' FC command on Test2 and Test3 to check they are same.
+@rem Copyright (C) Peter Johnson (www.delphidabbler.com), 2007-2009
 @rem
-@rem v1.0 of 18 Sep 2007 - First version.
-@rem v1.1 of 07 Apr 2008 - Revised to work with BPtch renamed from BPatch.
-@rem v1.2 of 14 Aug 2008 - Revised to work with "unrenamed" BPatch.
-@rem                     - Forced to work with exe files from Exe folder,
-@rem                       preventing calling of any other versions on path.
+@rem $Rev$
+@rem $Date$
 @rem ---------------------------------------------------------------------------
 
 
@@ -25,28 +14,86 @@
 
 setlocal 
 
-cd ..\Exe
-set ErrorMsg=
-set BDiff=..\Exe\BDiff.exe
-set BPatch=..\Exe\BPatch.exe
-set Test=..\Test
+rem Get directory containing BDiff and BPatch
+set ExeDir=..\Exe
+if not "%BDIFFPATH%" == "" set ExeDir=%BDIFFPATH%
+rem Record path to BDiff and BPatch
+set BDiff=%ExeDir%\BDiff.exe
+set BPatch=%ExeDir%\BPatch.exe
 
+rem Clear error message
+set ErrorMsg=
+
+rem Decide which test to run from command line
+if "%1" == "" goto usage
+if "%1" == "patch" goto DoPatchTest
+if "%1" == "quoted" goto DoQuotedTest
+if "%1" == "filtered" goto DoFilteredTest
+if "%1" == "version" goto DoVersionTest
+if "%1" == "clean" goto DoClean
+set ErrorMsg=Unknown test "%1".
+goto usage
+
+rem Run patch test
+:DoPatchTest
 echo --- Creating Patch with bdiff ---
-%BDiff% %Test%\Test1 %Test%\Test2 --output=%Test%\Patch --verbose -b
+%BDiff% Test1 Test2 --output=Patch --verbose -b
 if errorlevel 1 set ErrorMsg=BDiff failed
 if not "%ErrorMsg%"=="" goto error
 echo.
-
 echo --- Applying Patch with bpatch ---
-%BPatch% %Test%\Test1 %Test%\Test3 --input=%Test%\Patch
-if errorlevel 1 set ErrorMsg=BaPtch failed
+%BPatch% Test1 Test3 --input=Patch
+if errorlevel 1 set ErrorMsg=BPatch failed
 if not "%ErrorMsg%"=="" goto error
 echo.
-
 echo --- Testing patched file against original with fc ---
-fc %Test%\Test2 %Test%\Test3
+rem fc %Test%\Test2 %Test%\Test3
+fc Test2 Test3
 echo.
+goto end
 
+rem Run quoted diff test
+:DoQuotedTest
+%BDiff% --format=quoted --verbose Test1 Test2 >Diff
+if errorlevel 1 set ErrorMsg=BDiff failed
+if not "%ErrorMsg%"=="" goto error
+Notepad Diff
+goto end
+
+rem Run filtered diff test
+:DoFilteredTest
+%BDiff% --format=filtered --verbose Test1 Test2 >Diff
+if errorlevel 1 set ErrorMsg=BDiff failed
+if not "%ErrorMsg%"=="" goto error
+Notepad Diff
+goto end
+
+rem Run version number test
+:DoVersionTest
+%BDiff% --version
+if errorlevel 1 set ErrorMsg=BDiff failed
+if not "%ErrorMsg%"=="" goto error
+%BPatch% --version
+if errorlevel 1 set ErrorMsg=BPatch failed
+if not "%ErrorMsg%"=="" goto error
+goto end
+
+rem Remove generated files
+:DoClean
+del Test3 2>nul
+del Diff 2>nul
+del Patch 2>nul
+goto end
+
+:usage
+if not "%ErrorMsg%" == "" echo *** ERROR: %ErrorMsg%
+echo Usage is:
+echo   test.bat patch - test binary patching
+echo   test.bat quoted - create quoted text diff and display it
+echo   test.bat filtered - create filtered text diff and display it
+echo   test.bat version - display version information for BDiff and BPatch
+echo   test.bat clean - remove all generated files
+echo For more information see ReadMe.txt
 goto end
 
 :error
