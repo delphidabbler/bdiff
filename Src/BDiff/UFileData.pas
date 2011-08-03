cd4785f9e5d930fad179ec77d246357ffd493abe
DelphiDabbler <github@delphidabbler.com>
2011-08-03 10:21:46 +0000

    Added header comment.

--- Src/BDiff/UFileData.pas
+++ Src/BDiff/UFileData.pas
@@ -1,8 +1,28 @@
+{
+ * UFileData.pas
+ *
+ * Implements class that reads a file and provides access to its data.
+ *
+ * Copyright (c) 2011 Peter D Johnson (www.delphidabbler.com).
+ *
+ * $Rev$
+ * $Date$
+ *
+ * THIS SOFTWARE IS PROVIDED "AS-IS", WITHOUT ANY EXPRESS OR IMPLIED WARRANTY.
+ * IN NO EVENT WILL THE AUTHORS BE HELD LIABLE FOR ANY DAMAGES ARISING FROM THE
+ * USE OF THIS SOFTWARE.
+ *
+ * For conditions of distribution and use see the LICENSE file of visit
+ * http://www.delphidabbler.com/software/bdiff/license
+}
+
+
 unit UFileData;
 
 interface
 
 uses
+  // Project
   UBDiffTypes;
 
 type
@@ -24,7 +44,9 @@ type
 implementation
 
 uses
+  // Delphi
   SysUtils, Windows,
+  // Project
   UErrors;
 
 { TFileData }
